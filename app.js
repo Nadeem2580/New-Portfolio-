7c4daa6f4e9c32ce60d14e898f6eef587aeec627
Muhammad Nadeem <nadeemismail2580@gmail.com>
2025-04-09 20:18:53 +0500

    banner section done

---
+++ app.js
@@ -0,0 +1,7 @@
+// var typed = new typed(".texts" , {
+//     Strings:["Frontend Developer" , "youtube" , "Web Developer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay: 1000,
+//     loop:true  
+// })
