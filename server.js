93cb9919cc8f6ef66383a4e4ac0efc2731faa28a
Muhammad Nadeem <nadeemismail2580@gmail.com>
2025-04-19 18:39:32 +0500

    port adjust

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import nodemailer from "nodemailer"
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors())
